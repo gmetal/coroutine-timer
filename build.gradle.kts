d5b5bc74528039df8c08c0b6f35acd3fa282dc31
gmetal <gmetaxas@gmail.com>
2021-10-24 07:29:01 +0300

    Migrate the project to kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.0.0.M3")
+    testImplementation("io.kotest:kotest-assertions-core:5.0.0.M3")
     testImplementation("io.mockk:mockk:1.12.0")
 }
 
--- src/test/kotlin/CoroutineTimerTest.kt
+++ src/test/kotlin/CoroutineTimerTest.kt
@@ -1,88 +1,122 @@
+import io.kotest.common.ExperimentalKotest
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.test.delayController
+import io.kotest.matchers.shouldBe
 import io.mockk.*
-import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.runBlockingTest
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Timeout
-import java.lang.Thread.sleep
-import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
 import kotlin.test.assertEquals
 
+@ExperimentalStdlibApi
+@ExperimentalKotest
 @ExperimentalCoroutinesApi
-class CoroutineTimerTest {
+class CoroutineTimerTest : BehaviorSpec({
+    // Sets per-test case timeout for all test cases in the spec
+    timeout = 3500
+    // Enable the TestCoroutineDispatcher
+    testCoroutineDispatcher = true
 
-    @MockK
     lateinit var mockTimerAction: () -> Unit
-    lateinit var testCoroutineDispatcher: TestCoroutineDispatcher
+    lateinit var testCoroutineTimer: CoroutineTimer
 
-    @BeforeEach
-    fun setUp() {
-        MockKAnnotations.init(this)
+    fun setupTest(repeat: Boolean, coroutineContext: CoroutineContext) {
+        mockTimerAction = mockk()
         every { mockTimerAction.invoke() } just Runs
-        testCoroutineDispatcher = TestCoroutineDispatcher()
+
+        testCoroutineTimer = CoroutineTimer(
+            2000,
+            repeat,
+            timerAction = mockTimerAction,
+            dispatcher = coroutineContext[CoroutineDispatcher.Key]!!
+        )
     }
 
-    @Test
-    fun new_coroutine_timers_are_not_running() {
-        val testCoroutineTimer = CoroutineTimer(2000, false, timerAction = mockTimerAction)
+    Given("a CoroutineTimer") {
+        And("the timer executes once") {
+            And("the timer is not running ") {
+                When("the coroutine timer is initialised") {
+                    setupTest(repeat = false, coroutineContext)
+                    Then("it is not running") {
+                        testCoroutineTimer.isRunning() shouldBe false
+                    }
+                    Then("the timer action has not been executed") {
+                        verify { mockTimerAction wasNot called }
+                    }
+                }
+                When("the stop method is called") {
+                    setupTest(repeat = false, coroutineContext)
+                    testCoroutineTimer.stop()
 
-        assertEquals(false, testCoroutineTimer.isRunning())
-        verify { mockTimerAction wasNot called }
+                    Then("the timer is not running") {
+                        testCoroutineTimer.isRunning() shouldBe false
+                    }
+                    Then("the timer action is not executed") {
+                        verify { mockTimerAction wasNot called }
+                    }
+                }
+            }
+            And("the timer is running") {
+                When("the timer is executed") {
+                    setupTest(repeat = false, testContext.coroutineContext)
+                    testCoroutineTimer.start()
+                    val timerWasRunning = testCoroutineTimer.isRunning()
+
+                    delayController.advanceTimeBy(2100)
+
+                    Then("the timer was running") {
+                        timerWasRunning shouldBe true
+                    }
+                    Then("the timer stops running") {
+                        testCoroutineTimer.isRunning() shouldBe false
+                    }
+                    Then("the timerAction is executed once") {
+                        verify(atMost = 1, atLeast = 1) { mockTimerAction.invoke() }
+                    }
+                }
+                When("the timer is stopped") {
+                    setupTest(repeat = false, testContext.coroutineContext)
+                    testCoroutineTimer.start()
+
+                    delayController.advanceTimeBy(1000)
+
+                    val timerWasRunning = testCoroutineTimer.isRunning()
+
+                    testCoroutineTimer.stop()
+                    delayController.advanceTimeBy(1500)
+
+                    Then("verify the time was running") {
+                        timerWasRunning shouldBe true
+                    }
+                    Then("the timer stops running") {
+                        assertEquals(false, testCoroutineTimer.isRunning())
+                    }
+                    Then("the timer actions is not executed") {
+                        verify { mockTimerAction wasNot called }
+                    }
+                }
+            }
+        }
+        And("the timer executes indefinitely") {
+            When("a single time period has elapsed") {
+                setupTest(repeat = true, testContext.coroutineContext)
+                testCoroutineTimer.start()
+                delayController.advanceTimeBy(2100)
+                val timerWasRunning = testCoroutineTimer.isRunning()
+                delayController.advanceTimeBy(2100)
+
+                testCoroutineTimer.stop()
+
+                Then("the timer was running") {
+                    timerWasRunning shouldBe true
+                }
+                Then("the timer action has executed twice") {
+                    verify(atLeast = 2, atMost = 2) { mockTimerAction.invoke() }
+                }
+                Then("the timer is not running") {
+                    testCoroutineTimer.isRunning() shouldBe false
+                }
+            }
+        }
     }
-
-    @Test
-    fun on_new_coroutine_timers_the_stop_method_does_nothing() {
-        val testCoroutineTimer = CoroutineTimer(2000, false, timerAction = mockTimerAction)
-
-        testCoroutineTimer.stop()
-
-        assertEquals(false, testCoroutineTimer.isRunning())
-        verify { mockTimerAction wasNot called }
-    }
-
-    @Timeout(3500, unit = TimeUnit.MILLISECONDS)
-    @Test
-    fun without_repeat_calling_start_will_execute_the_timer_action_once() = runBlockingTest(testCoroutineDispatcher) {
-        val testCoroutineTimer = CoroutineTimer(2000, false, testCoroutineDispatcher, mockTimerAction)
-
-        testCoroutineTimer.start()
-
-        assertEquals(true, testCoroutineTimer.isRunning())
-        advanceTimeBy(2100)
-
-        assertEquals(false, testCoroutineTimer.isRunning())
-        verify { mockTimerAction.invoke() }
-    }
-
-    @Timeout(3500, unit = TimeUnit.MILLISECONDS)
-    @Test
-    fun without_repeat_calling_stop_will_stop_the_timer_without_executing_the_timer_action() = runBlockingTest(testCoroutineDispatcher) {
-        val testCoroutineTimer = CoroutineTimer(2000, false, testCoroutineDispatcher, mockTimerAction)
-
-        testCoroutineTimer.start()
-        advanceTimeBy(1000)
-        assertEquals(true, testCoroutineTimer.isRunning())
-        testCoroutineTimer.stop()
-        advanceTimeBy(1500)
-
-        assertEquals(false, testCoroutineTimer.isRunning())
-        verify { mockTimerAction wasNot called }
-    }
-
-    @Test
-    fun with_repeat_wait_to_execute_twice_and_then_stop() = runBlockingTest(testCoroutineDispatcher) {
-        val testCoroutineTimer = CoroutineTimer(2000, true, testCoroutineDispatcher, mockTimerAction)
-
-        testCoroutineTimer.start()
-        advanceTimeBy(2100)
-        assertEquals(true, testCoroutineTimer.isRunning())
-        verify(atLeast = 1, atMost = 1) { mockTimerAction.invoke() }
-        advanceTimeBy(2100)
-        testCoroutineTimer.stop()
-        verify(atLeast = 2, atMost = 2) { mockTimerAction.invoke() }
-
-        assertEquals(false, testCoroutineTimer.isRunning())
-    }
-}
+})
